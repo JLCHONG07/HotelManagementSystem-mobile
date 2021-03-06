9f9574b0406704228839a57c6c0f50bda3837967
jiale <jiale8432@gmail.com>
2021-03-06 17:23:05 +0800

    main activity for all modules

--- app/src/main/java/com/example/hotelmanagementsystem_mobile/Account.kt
+++ app/src/main/java/com/example/hotelmanagementsystem_mobile/Account.kt
@@ -3,9 +3,9 @@ package com.example.hotelmanagementsystem_mobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Account : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_account)
     }
 }
