145815ffcce855f528127c2b1f9d21d92a8b611d
gahseng_2020 <gahsengf@gmail.com>
2021-03-14 14:20:10 +0800

    Custom AlertDialog in Check In

--- app/src/main/java/com/example/hotelmanagementsystem_mobile/activities/CheckInActivity.kt
+++ app/src/main/java/com/example/hotelmanagementsystem_mobile/activities/CheckInActivity.kt
@@ -1,9 +1,14 @@
 package com.example.hotelmanagementsystem_mobile.activities
 
+import android.content.Context
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hotelmanagementsystem_mobile.R
 import com.example.hotelmanagementsystem_mobile.adapters.CheckInDetailsAdapter
@@ -11,6 +16,9 @@ import com.example.hotelmanagementsystem_mobile.models.*
 import kotlinx.android.synthetic.main.activity_check_in.*
 
 class CheckInActivity : AppCompatActivity() {
+    private lateinit var alertDialog: AlertDialog
+    private var checkInDetailsList : ArrayList<CheckIn> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_in)
@@ -19,7 +27,10 @@ class CheckInActivity : AppCompatActivity() {
 
         val typeface = Typeface.createFromAsset(assets, "BerkshireSwash-Regular.ttf")
         tv_check_in_title.typeface = typeface
+    }
 
+    override fun onStart() {
+        super.onStart()
         getCheckInDetails()
     }
 
@@ -36,9 +47,8 @@ class CheckInActivity : AppCompatActivity() {
     }
 
     private fun getCheckInDetails() {
-        var checkInDetailsList : ArrayList<CheckIn> = ArrayList()
-        checkInDetailsList.add(CheckIn("RSVTN0001", 3, 2, 2, 4, "Quest Room", "2 February 2021, 2:30pm", "checkin0001"))
-        checkInDetailsList.add(CheckIn("RSVTN0001", 3, 2, 2, 4, "Quest Room", "2 February 2021, 2:30pm", "checkin0001"))
+        checkInDetailsList.add(CheckIn("RSVTN0001", 3, 2, 2, 4, "Quest Room", "checkin0001", "2 February 2021, 2:30pm"))
+        checkInDetailsList.add(CheckIn("RSVTN0001", 3, 2, 2, 4, "Quest Room", "checkin0001", "2 February 2021, 2:30pm"))
 
         rv_check_in_details.visibility = View.VISIBLE
         rv_check_in_details.layoutManager = LinearLayoutManager(this)
@@ -46,5 +56,47 @@ class CheckInActivity : AppCompatActivity() {
 
         val adapter = CheckInDetailsAdapter(this, checkInDetailsList)
         rv_check_in_details.adapter = adapter
+    }
+
+    fun showCustomAlertDialog(position: Int) {
+        Log.i("Position", position.toString())
+        Log.i("Position", checkInDetailsList[position].toString())
+            val inflater: LayoutInflater = this.layoutInflater
+            val dialogView: View = inflater.inflate(R.layout.check_in_out_details_dialog_view, null)
+            var textNights =
+                "${checkInDetailsList[position].numberOfDays} Days, ${checkInDetailsList[position].numberOfNights} Nights"
+            var textReservationDetails =
+                "${checkInDetailsList[position].numberOfRooms} Rooms, ${checkInDetailsList[position].numberOfQuests} Guests"
+
+            val textViewReservationNumber =
+                dialogView.findViewById<TextView>(R.id.check_in_details_dialog_reservation_number)
+            textViewReservationNumber?.text = checkInDetailsList[position].reservationNumber
+
+            val textViewNights =
+                dialogView.findViewById<TextView>(R.id.check_in_details_dialog_nights)
+            textViewNights?.text = textNights
+
+            val textViewReservationDetails =
+                dialogView.findViewById<TextView>(R.id.check_in_details_dialog_reservation_details)
+            textViewReservationDetails?.text = textReservationDetails
+
+            val textViewRoomTypes =
+                dialogView.findViewById<TextView>(R.id.check_in_details_dialog_room_types)
+            textViewRoomTypes?.text = checkInDetailsList[position].roomTypes
+
+            val textViewDateAndTime =
+                dialogView.findViewById<TextView>(R.id.check_in_details_dialog_date_time)
+            textViewDateAndTime?.text = checkInDetailsList[position].checkInDateAndTime
+
+            val textViewCheckInId =
+                dialogView.findViewById<TextView>(R.id.check_in_details_dialog_check_in_id)
+            textViewCheckInId?.text = checkInDetailsList[position].checkInId
+
+            val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+            dialogBuilder.setView(dialogView)
+            alertDialog = dialogBuilder.create()
+            alertDialog.show()
+        Log.i("AlertDialog", alertDialog.isShowing.toString())
     }
 }
--- app/src/main/java/com/example/hotelmanagementsystem_mobile/adapters/CheckInDetailsAdapter.kt
+++ app/src/main/java/com/example/hotelmanagementsystem_mobile/adapters/CheckInDetailsAdapter.kt
@@ -1,15 +1,17 @@
 package com.example.hotelmanagementsystem_mobile.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hotelmanagementsystem_mobile.R
+import com.example.hotelmanagementsystem_mobile.activities.CheckInActivity
 import com.example.hotelmanagementsystem_mobile.models.CheckIn
 import kotlinx.android.synthetic.main.item_check_in_details.view.*
 
-open class CheckInDetailsAdapter(private val context: Context, private var list: ArrayList<CheckIn>)
+open class CheckInDetailsAdapter(private val context: CheckInActivity, private var list: ArrayList<CheckIn>)
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_check_in_details, parent, false))
@@ -17,10 +19,15 @@ open class CheckInDetailsAdapter(private val context: Context, private var list:
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val model = list[position]
+        Log.i("Position", position.toString())
         if(holder is MyViewHolder) {
             holder.itemView.tv_check_in_id.text = model.checkInId
             holder.itemView.tv_check_in_number_of_room.text = model.numberOfRooms.toString()
             holder.itemView.tv_check_in_number_of_customer.text = model.numberOfQuests.toString()
+
+            holder.itemView.cv_check_in_details.setOnClickListener {
+                context.showCustomAlertDialog(position)
+            }
         }
     }
 
