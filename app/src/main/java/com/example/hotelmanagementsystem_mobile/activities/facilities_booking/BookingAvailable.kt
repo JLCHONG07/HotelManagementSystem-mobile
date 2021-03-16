6980a4af88f99cb7bbb49aac66f4d8f85f7963e6
jiale <jiale8432@gmail.com>
2021-03-16 13:54:26 +0800

    book available, date picker

--- app/src/main/java/com/example/hotelmanagementsystem_mobile/activities/facilities_booking/BookingAvailable.kt
+++ app/src/main/java/com/example/hotelmanagementsystem_mobile/activities/facilities_booking/BookingAvailable.kt
@@ -1,20 +1,37 @@
 package com.example.hotelmanagementsystem_mobile.activities.facilities_booking
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.DatePicker
 import android.widget.TextView
 import android.widget.Toolbar
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.cardview.widget.CardView
 import com.example.hotelmanagementsystem_mobile.R
 import com.example.hotelmanagementsystem_mobile.activities.Homepage
 import kotlinx.android.synthetic.main.activity_booking_available.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-class BookingAvailable : AppCompatActivity(), View.OnClickListener {
+class BookingAvailable : AppCompatActivity(), View.OnClickListener,DatePickerDialog.OnDateSetListener {
+
+    var day=0;
+    var month=0;
+    var year=0;
+
+    var savedDay=0;
+    var savedMonth=0;
+    var savedYear=0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking_available)
@@ -33,7 +50,10 @@ class BookingAvailable : AppCompatActivity(), View.OnClickListener {
         cardView180Minutes.setOnClickListener(this)
 
 
+
+        pickDate()
     }
+
 
     /* Back to previous activity*/
     override fun onSupportNavigateUp(): Boolean {
@@ -73,5 +93,33 @@ class BookingAvailable : AppCompatActivity(), View.OnClickListener {
         txtViewNum2.setTextColor(Color.parseColor("#F95F62"))
         txtViewNum3.setTextColor(Color.parseColor("#F95F62"))
 
+    }
+    private fun pickDate(){
+
+
+
+        btnDate.setOnClickListener{
+            getDateCalender()
+
+            DatePickerDialog(this,this,year,month,day).show()
+
+        }
+        
+    }
+
+    private fun getDateCalender(){
+        val cal=Calendar.getInstance()
+        day=cal.get(Calendar.DAY_OF_MONTH)
+        month=cal.get(Calendar.MONTH)
+        year=cal.get(Calendar.YEAR)
+        
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedDay=dayOfMonth
+        savedMonth=month
+        savedYear=year
+
+        btnDate.text="$savedDay/$savedMonth/$savedYear"
     }
 }
