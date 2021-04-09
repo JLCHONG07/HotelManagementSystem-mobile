dd4ea532ce7d9546a82b78399db285981392243f
jiale <jiale8432@gmail.com>
2021-04-09 10:23:32 +0800

    fully done facilities booking ui and navigator

--- app/src/main/java/com/example/hotelmanagementsystem_mobile/activities/facilities_booking/SummaryBookDetails.kt
+++ app/src/main/java/com/example/hotelmanagementsystem_mobile/activities/facilities_booking/SummaryBookDetails.kt
@@ -1,12 +1,55 @@
 package com.example.hotelmanagementsystem_mobile.activities.facilities_booking
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
 import com.example.hotelmanagementsystem_mobile.R
+import com.example.hotelmanagementsystem_mobile.activities.Homepage
+import kotlinx.android.synthetic.main.activity_summary_book_details.*
 
-class SummaryBookDetails : AppCompatActivity() {
+class SummaryBookDetails : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var alertDialog: AlertDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary_book_details)
+
+        btnConfirm.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnConfirm -> {
+                showSuccessfulAlertBox()
+            }
+
+
+        }
+    }
+
+    private fun showSuccessfulAlertBox() {
+        val inflater: LayoutInflater = this.layoutInflater
+        val dialogView: View = inflater.inflate(R.layout.successful_book_msg, null)
+
+
+        val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+        dialogBuilder.setView(dialogView)
+
+        dialogView.findViewById<Button>(R.id.btnOK).setOnClickListener {
+            val intent= Intent(this,Homepage::class.java)
+            intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+        alertDialog = dialogBuilder.create()
+        alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent);
+        alertDialog.show()
+
+
+
+
     }
 }
