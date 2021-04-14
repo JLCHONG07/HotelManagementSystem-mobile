094aae28e2c40bf1d4a4dc2f18ca87a18d2ef623
Alvin Ling <alvinling120@gmail.com>
2021-04-14 15:11:07 +0800

    Added voucher firebase attributes

--- app/src/main/java/com/example/hotelmanagementsystem_mobile/activities/EVouchers.kt
+++ app/src/main/java/com/example/hotelmanagementsystem_mobile/activities/EVouchers.kt
@@ -2,10 +2,13 @@ package com.example.hotelmanagementsystem_mobile.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.hotelmanagementsystem_mobile.R
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hotelmanagementsystem_mobile.adapters.VoucherRecycleAdapter
 import com.example.hotelmanagementsystem_mobile.models.ModelVoucher
+import com.example.hotelmanagementsystem_mobile.models.TimeSlot
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_categories.*
 import kotlinx.android.synthetic.main.activity_e_vouchers.*
 
@@ -15,22 +18,57 @@ class EVouchers : AppCompatActivity() {
         setContentView(R.layout.activity_e_vouchers)
 
         val arrayListVoucher = ArrayList<ModelVoucher>()
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,60,"Table Tennis","TT123456","SPORTS"));
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,120,"Badminton","BMT12345","SPORTS"));
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,180,"Snooker","SNK12345","SPORTS"));
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,60," ","GR123456","GAMING ROOM"));
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,120," ","GR123456","GAMING ROOM"));
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,180," ","GR123456","GAMING ROOM"));
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,60," ","BG123456","BOARD GAME"));
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,120," ","BG123456","BOARD GAME"));
-        arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,180," ","BG123456","BOARD GAME"));
+        retriveVoucher(arrayListVoucher)
 
 
+        // val arrayListVoucher = ArrayList<ModelVoucher>()
+        // arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,60,"Table Tennis","TT123456","SPORTS"));
+        // arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,120,"Badminton","BMT12345","SPORTS"));
+        // arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,180,"Snooker","SNK12345","SPORTS"));
+        //  arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,60," ","GR123456","GAMING ROOM"));
+        //  arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,120," ","GR123456","GAMING ROOM"));
+        //  arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,180," ","GR123456","GAMING ROOM"));
+        //  arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,60," ","BG123456","BOARD GAME"));
+        //  arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,120," ","BG123456","BOARD GAME"));
+        //  arrayListVoucher.add(ModelVoucher(R.drawable.e_voucher,180," ","BG123456","BOARD GAME"));
 
 
-        val voucherRecycleAdapter = VoucherRecycleAdapter(arrayListVoucher,this@EVouchers)
+    }
 
-        recycleViewVoucher.layoutManager=LinearLayoutManager(this)
-        recycleViewVoucher.adapter=voucherRecycleAdapter
+    private fun assignVoucher(arrayListVoucher: ArrayList<ModelVoucher>) {
+        val voucherRecycleAdapter = VoucherRecycleAdapter(arrayListVoucher, this@EVouchers)
+
+        recycleViewVoucher.layoutManager = LinearLayoutManager(this)
+        recycleViewVoucher.adapter = voucherRecycleAdapter
+
+    }
+
+    private fun retriveVoucher(arrayListVoucher: ArrayList<ModelVoucher>) {
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("e_voucher")
+            .get()
+            .addOnSuccessListener {
+
+                var counter = 0
+                for (i in it.documents) {
+
+                    arrayListVoucher.add(
+                        ModelVoucher(
+                            R.drawable.e_voucher,
+                            it.documents[counter].data?.get("timeDuration") as String,
+                            it.documents[counter].data?.get("vouchType") as String,
+                            it.documents[counter].data?.get("vouchCode") as String,
+                            it.documents[counter].data?.get("vouchCat") as String
+                        )
+                    )
+                    counter++;
+                }
+
+                assignVoucher(arrayListVoucher)
+
+            }
+
+
     }
 }
--- app/src/main/java/com/example/hotelmanagementsystem_mobile/models/Model.kt
+++ app/src/main/java/com/example/hotelmanagementsystem_mobile/models/Model.kt
@@ -2,4 +2,4 @@ package com.example.hotelmanagementsystem_mobile.models
 
 
 
-data class ModelVoucher(val _image:Int,val timeDuration:Int, val vouchType:String, val vouchCode:String,val vouchCat: String )
+data class ModelVoucher(val _image:Int,val timeDuration:String, val vouchType:String, val vouchCode:String,val vouchCat: String )
